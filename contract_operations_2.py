7985ba2f3e2363c181366f2b46d6d27ae62de997
hclivess <admin@bismuth.cz>
2021-11-20 14:22:56 -0600

    add examples

--- contract_operations_2.py
+++ contract_operations_2.py
@@ -4,23 +4,23 @@ import requests
 decimals = 1000000000000000000
 
 contract = "KT1PxkrCckgh5fA5v2cZEE2bX5q2RV1rv8dj"
-level=""
+level = ""
 url = f"https://api.better-call.dev/v1/contract/mainnet/{contract}/operations?level={level}"
 
 result = json.loads(requests.get(url).text)
-entry_filter = ["buy", "sell"]
+filter = ["mint", "burn"]
 
 operations = result["operations"]
 
 for_output = []
 
 for operation in operations:
-    if operation["entrypoint"] in entry_filter:
-        #print(operation)
+    if operation["entrypoint"] in filter:
+        # print(operation)
         print(f'{operation["source"]} applied '
               f'{operation["entrypoint"]} of '
-              f'{operation["parameters"][0]["children"][0]["value"]}'
-              f', which is {int(operation["parameters"][0]["children"][0]["value"])/decimals}')
+              f'{operation["parameters"][0]["children"][1]["value"]}'
+              f', which is {int(operation["parameters"][0]["children"][1]["value"]) / decimals}')
 
         for_output.append(operation)
 
